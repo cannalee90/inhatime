f26e960fb55e071ba0207e0d8980eb701054e6c0
cannalee90 <cannalee90@gmail.com>
2017-03-30 22:07:19 +0900

    add basic element

---
+++ frontend/react/components/btn.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Btn = (props) => {
+  const { className, onClick, type, value } = props;
+  return (
+    <button
+      className={className}
+      onClick={onClick}
+      type={type}
+    >
+      {value}
+    </button>
+  );
+};
+
+Btn.defaultProps = {
+  type: 'button',
+  value: 'value',
+  className: '',
+};
+
+Btn.propTypes = {
+  className: React.PropTypes.string,
+  onClick: React.PropTypes.func.isRequired,
+  type: React.PropTypes.string,
+  value: React.PropTypes.string,
+};
+
+export default Btn;
