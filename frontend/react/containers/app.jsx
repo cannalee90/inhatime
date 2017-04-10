88e8bc8fa600a334c3075a05603f10399d804fc5
cannalee90 <cannalee90@gmail.com>
2017-04-11 00:45:50 +0900

    css file 수정 및 적용을 한군대에서 하도록 수정

--- frontend/react/containers/app.jsx
+++ frontend/react/containers/app.jsx
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import Nav from './../components/nav';
 import Footer from './../components/footer';
-import '../styles/style.css';
+import './../styles/style.css';
+import './../styles/custom.scss';
 
 export default class App extends Component {
   constructor(props) {
