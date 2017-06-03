9130da285e3e0ffeb56bd4d63d5048ea7773e9bf
cannalee90 <cannalee90@gmail.com>
2017-06-03 23:33:11 +0900

    user-signup page

--- frontend/react/containers/user-signup/index.jsx
+++ frontend/react/containers/user-signup/index.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
+
 import AppAlert from 'Components/app-alert';
 
 import UserSignupForm from './user-signup-form';
-import { userSignup, clearErrors } from './../../actions/session';
-import ErrorRender from './../../components/error-render';
 
 class UserSignin extends Component {
   constructor(props) {
@@ -15,11 +15,10 @@ class UserSignin extends Component {
   }
 
   componentWillUnmount() {
-    this.props.clearErrors();
   }
 
   onSubmit(values) {
-    const { email, password, department } = values
+    const { email, password, department } = values;
     this.props.userSignup({ email, password, MajorId: department.value });
   }
 
@@ -37,18 +36,17 @@ class UserSignin extends Component {
     // const { errors } = this.props.session;
     const { departments } = this.props.app;
     return (
-      <div className='container content'>
-        <div className='row'>
-          <div className='col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3'>
-            <AppAlert
-              state={this.props.app}
-            />
-            <UserSignupForm
-              onSubmit={this.onSubmit}
-              errorRender={false}
-              departments={this.renderDepartment(departments)}
-            />
-          </div>
+      <div className='container-fluid user-page-wrapper'>
+        <div className='form-wrapper'>
+          <h3 className='header'>획원가입</h3>
+          <AppAlert
+            state={this.props.app}
+          />
+          <UserSignupForm
+            onSubmit={this.onSubmit}
+            errorRender={false}
+            departments={this.renderDepartment(departments)}
+          />
         </div>
       </div>
     );
@@ -63,14 +61,13 @@ const mapStateToProps = ({ session, app }) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ userSignup, clearErrors }, dispatch);
+  return bindActionCreators({}, dispatch);
 };
 
 UserSignin.propTypes = {
+  app: PropTypes.object.isRequired,
   userSignup: PropTypes.func.isRequired,
-  session: PropTypes.object.isRequired,
-  clearErrors: PropTypes.func.isRequired,
+  // session: PropTypes.object.isRequired,
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserSignin);
